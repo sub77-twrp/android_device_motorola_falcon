f71e2339e0c1afd1667dff31a2992ebc717c3814
sub77 <sub77@ymail.com>
2016-06-29 20:19:21 -0500

    multirom: Add QCOM Overlay support
    Change-Id: I0875afd2d659ea21990eec4a72784299e71d6ed3

--- multirom/BoardConfigMultirom.mk
+++ multirom/BoardConfigMultirom.mk
@@ -16,3 +16,9 @@ MR_DEVICE_HOOKS := device/motorola/falcon/multirom/mr_hooks.c
 MR_DEVICE_HOOKS_VER := 4
 MR_FSTAB := device/motorola/falcon/multirom/mrom.fstab
 MR_INIT_DEVICES := device/motorola/falcon/multirom/mr_init_devices.c
+
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+MR_USE_QCOM_OVERLAY := true
+MR_PIXEL_FORMAT := "RGBX_8888"
+MR_QCOM_OVERLAY_HEADER := device/motorola/falcon/multirom/overlay/mr_qcom_overlay.h
+MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
